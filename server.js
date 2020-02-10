a7d9a98e9a38a1ffa1834921b8dc5e8eaa986129
Valdo99 <edvaldogjonikaj@cln-239-216.eduroam.unimi.it>
2020-02-10 10:12:27 +0100

    bulk insert

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 4000;
 let Song = require('./songModel.js');
 app.use(cors());
 app.use(bodyParser.json());
-mongoose.connect('mongodb://127.0.0.1:27017/Songs', {useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/Songs', {useUnifiedTopology: true,   useNewUrlParser: true });
 const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
@@ -55,6 +55,16 @@ songRoutes.route('/add').post(function(req, res) {
             res.status(400).send('adding new song failed');
         });
 });
+songRoutes.route('/add/bulk').post(function(req, res) {
+    Song.insertMany(req.body, function(error, docs) {}).then(song => {
+        res.status(200).json({'song': 'song added successfully'});
+    })
+    .catch(err => {
+        res.status(400).send('adding new song failed');
+    });
+});
+
+
 app.use('/Songs', songRoutes);
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
