cb1bea2a3cadb29a1b0bc67a0d81d23bb836841f
Valdo99 <edvaldogjonikaj@cln-239-216.eduroam.unimi.it>
2020-02-10 08:25:29 +0100

    useUnifiedTopology: true

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = 4000;
 let Song = require('./songModel.js');
 app.use(cors());
 app.use(bodyParser.json());
-mongoose.connect('mongodb://127.0.0.1:27017/Songs', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/Songs', {useUnifiedTopology: true, useNewUrlParser: true });
 const connection = mongoose.connection;
 connection.once('open', function() {
     console.log("MongoDB database connection established successfully");
